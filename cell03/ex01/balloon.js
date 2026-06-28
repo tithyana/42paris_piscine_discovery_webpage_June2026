3d24bf24abd5d95eb1c29d18b12f3b19beb0fdf3
tithyana <yanacurry@yahoo.com>
2026-06-28 23:20:57 +0200

    cell3.01

--- cell03/ex01/balloon.js
+++ cell03/ex01/balloon.js
@@ -5,16 +5,50 @@ function myFunction() {
     
     const currWidth = myDiv.clientWidth;
     const currHeight = myDiv.clientHeight;
-    myDiv.style.width = currWidth + 10 + "px";
-    myDiv.style.height = currHeight + 10 + "px";
+    
+    if (currWidth >= 420) {
+        myDiv.style.width = 200 + "px";
+        myDiv.style.height = 200 + "px"; 
+    } else {
+        myDiv.style.width = currWidth + 10 + "px";
+        myDiv.style.height = currHeight + 10 + "px";
+    }
 
     console.log("cc")
 
     color = colors.shift();
     colors.push(color);
+
+
+    if (color === myDiv.style.backgroundColor) {
+       color = colors.shift();
+        colors.push(color); 
+    }
     
     myDiv.style.backgroundColor = color;
 }
 
+function mouseLeave() {
+    console.log("mouseleave")
 
+    const myDiv = document.querySelector(".balloon");
+    
+    const currWidth = myDiv.clientWidth;
+    const currHeight = myDiv.clientHeight;
+    
+    if (currWidth > 200) {
+        myDiv.style.width = currWidth - 5 + "px";
+        myDiv.style.height = currHeight - 5 + "px";
+    }
+
+    color = colors.pop();
+    colors.unshift(color);
+
+    if (color === myDiv.style.backgroundColor) {
+        color = colors.pop();
+        colors.unshift(color); 
+    }
+
+    myDiv.style.backgroundColor = color;
+}
 
